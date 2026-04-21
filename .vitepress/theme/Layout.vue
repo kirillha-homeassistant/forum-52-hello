068efc20c24a670b779995e184a327b4ae10dec7
kirillha-homeassistant <kayanshin@yandex.ru>
2026-04-21 11:31:25 +0700

    Create Layout.vue

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,31 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import { useData } from 'vitepress'
+
+const { Layout } = DefaultTheme
+const { isDark } = useData()
+</script>
+
+<template>
+  <Layout>
+    <template #doc-after>
+      <div style="margin-top: 24px;">
+        <script
+          src="https://giscus.app/client.js"
+          data-repo="kirillha-homeassistant/forum-52-hello"
+          data-repo-id="R_kgDOSIOv4g"
+          data-category-id="DIC_kwDOSIOv4s4C7UrW"
+          data-mapping="pathname"
+          data-strict="0"
+          data-reactions-enabled="1"
+          data-emit-metadata="0"
+          data-input-position="top"
+          data-theme="transparent_dark"
+          data-lang="ru"
+          crossorigin="anonymous"
+          async
+        ></script>
+      </div>
+    </template>
+  </Layout>
+</template>
