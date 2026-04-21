3996a1ff1989d792735100411963cdc2e34b5df3
kirillha-homeassistant <kayanshin@yandex.ru>
2026-04-21 11:01:27 +0700

    Update index.ts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,5 +1,14 @@
-import { VPCarbon } from 'vitepress-carbon'
-
-// uncomment to test CSS variables override
-// import './override.css'
-export default VPCarbon
+<script src="https://giscus.app/client.js"
+        data-repo="kirillha-homeassistant/forum-52-hello"
+        data-repo-id="R_kgDOSIOv4g"
+        data-category-id="DIC_kwDOSIOv4s4C7UrW"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="top"
+        data-theme="preferred_color_scheme"
+        data-lang="ru"
+        crossorigin="anonymous"
+        async>
+</script>
